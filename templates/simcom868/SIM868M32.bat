9aadaa69d369501ae66238df6db3627e00bc12ff
umesh@work <beyondlogic2012@gmail.com>
2021-06-05 14:16:58 +0530

    updated SIM868M32.bat, added --silent option. this will prevent traces of makefile execution

--- templates/simcom868/SIM868M32.bat
+++ templates/simcom868/SIM868M32.bat
@@ -1,5 +1,5 @@
-rem CMD ,run "SIM800C32.bat all" or "SIM800C32.bat clean"
 @echo off
+rem CMD ,run "SIM800C32.bat all" or "SIM800C32.bat clean"
 set path=%CD%\build\winmake;%path%
 rem make -f build\Makefile --debug=b PROJ=%~n0 CFG=user %1
 make -f build\Makefile --silent PROJ=%~n0 CFG=user %1
